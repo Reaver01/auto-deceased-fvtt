d730155dc2a218e8f4604372935bed6f53a109b8
Reaver01 <jawmac@gmail.com>
2019-10-15 23:36:29 -0600

    version 0.2

--- auto-deceased/auto-deceased.js
+++ auto-deceased/auto-deceased.js
@@ -1,6 +1,6 @@
 // @author Reaver#4634
 
-Hooks.on('preUpdateToken', tokenInfo => {
+Hooks.on('updateToken', tokenInfo => {
     if (!tokenInfo.scene.active) {
         return;
     }
@@ -9,9 +9,7 @@ Hooks.on('preUpdateToken', tokenInfo => {
         activeCombat = game.combats.entities.filter(combat => combat.data.active)[0];
         combatToken = activeCombat.data.combatants.filter(token => token.id == tokenInfo.data.id)[0];
         if (!combatToken.defeated) {
-            index1 = game.combats.index(activeCombat.data._id);
-            index2 = game.combats.entities[index1].data.combatants.findIndex(token => token.id == tokenInfo.data.id);
-            game.combats.entities[index1].data.combatants[index2].defeated = true;
+            game.combat.updateCombatant({id: tokenInfo.data.id, defeated: true});
         }
     }
 });
